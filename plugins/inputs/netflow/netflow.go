f571eb726bc634c485b659e28906d7c911fa95ee
Marcus Boden <mboden@gwdg.de>
2017-07-11 10:14:50 +0200

    completed

--- plugins/inputs/netflow/netflow.go
+++ plugins/inputs/netflow/netflow.go
@@ -1,47 +1,108 @@
-package main
+package netflow
 
 import (
-  "fmt"
-  "log"
-  "github.com/gophercloud/gophercloud"
-  "github.com/gophercloud/gophercloud/openstack"
-  "github.com/gophercloud/gophercloud/pagination"
-  "github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+    "fmt"
+    "strings"
+    "net"
+    "bufio"
+    "github.com/influxdata/telegraf"
+    "github.com/influxdata/telegraf/plugins/inputs"
+    "errors"
+    "strconv"
+    "github.com/gophercloud/gophercloud"
+    "github.com/gophercloud/gophercloud/openstack"
+    "github.com/gophercloud/gophercloud/pagination"
+    "github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
 )
 
-func main() {
-  opts := gophercloud.AuthOptions{
-    IdentityEndpoint: "https://cloud.gwdg.de:5000/",
-    Username: "admin",
-    Password: "QjtTCfTj87Gq",
-    TenantName: "admin",
-  }
-  provider, err := openstack.AuthenticatedClient(opts)
-  if err != nil {
-    log.Fatal(err)
-    return
-  }
-  otheropts := gophercloud.EndpointOpts{Region: "RegionOne"}
-  client, err := openstack.NewComputeV2(provider, otheropts)
-  if err != nil {
-    log.Fatal(err)
-    return
-  }
-  opts2 := servers.ListOpts{AllTenants: true}
-  pager := servers.List(client, opts2)
-  pager.EachPage(func(page pagination.Page) (bool, error) {
-    serverList, err := servers.ExtractServers(page)
+type Netflow struct {
+    IdentityEndpoint   string
+    Username        string
+    Password        string
+    TenantName string
+}
 
-    fmt.Printf("size: %d\n", len(serverList)) 
+var sampleConfig = `
+  IdentityEndpoint = ""
+  username = "myuser"
+  password = "mypassword"
+  tenantname = "myuser"
+`
+
+func (n *Netflow) Description() string {
+    return "Plugin to collect netflow data with pmacct and aggregate some metrics"
+}
+
+func (n *Netflow) SampleConfig() string {
+    return sampleConfig
+}
+func (n *Netflow) GetIPUuidMappings() map[string]string {
+    opts := gophercloud.AuthOptions{
+        IdentityEndpoint: n.IdentityEndpoint,
+        Username: n.Username,
+        Password: n,Password,
+        TenantName: n.TenantName,
+    }
+    provider, err := openstack.AuthenticatedClient(opts)
     if err != nil {
-      log.Fatal(err)
-      return false, nil
+        return err
     }
-    for _, s := range serverList {
-
-      fmt.Println(s.ID, s.Name, s.Status)
-
+    otheropts := gophercloud.EndpointOpts{Region: n.Region}
+    client, err := openstack.NewComputeV2(provider, otheropts)
+    if err != nil {
+        return err
     }
-    return true, nil
-  })
+    opts2 := servers.ListOpts{AllTenants: true}
+    pager := servers.List(client, opts2)
+    uuidIPMap := make(map[string]string)
+    pager.EachPage(func(page pagination.Page) (bool, error) {
+        serverList, err := servers.ExtractServers(page)
+
+        fmt.Printf("size: %d\n", len(serverList)) 
+        if err != nil {
+            log.Fatal(err)
+            return false, nil
+        }
+        for _, s := range serverList {
+
+            fmt.Println(s.ID)
+            for _, v:= range s.Addresses {
+                for _,n := range v.([]interface{}) {
+                    add, _ := n.(map[string]interface{})
+                    if add["OS-EXT-IPS:type"] == "floating" {
+                        uuidIPMap[s.ID] = add["addr"].(string)
+                    }
+                }
+            }
+        }
+        return true, nil
+    })
+    return uuidIPMap
+}
+
+func (n *Netflow) Gather(acc telegraf.Accumulator) error {
+//    users := t.GetUserInfo()
+//    for _,u := range users {
+//        if c, _ := strconv.Atoi(u["client_type"]); c == 0 {
+//            tags := map[string]string{
+//                "unique_identifier" : u["client_unique_identifier"],
+//                "input_muted" : u["client_input_muted"],
+//                "away" : u["client_away"],
+//                "output_muted" : u["client_output_muted"],
+//            }
+//            values := map[string]interface{}{
+//                "nickname" : u["client_nickname"],
+//                "packets_received_total" : u["connection_packets_received_total"],
+//                "packets_sent_total" : u["connection_packets_sent_total"],
+//                "ip" : u["connection_client_ip"],
+//                "lastconnected" : u["client_lastconnected"],
+//            }
+//            acc.AddFields("user", values, tags)
+//        }
+//    }
+    return nil
+}
+
+func init() {
+    inputs.Add("netflow", func() telegraf.Input { return &Netflow{} })
 }
